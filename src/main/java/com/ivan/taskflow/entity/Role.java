cbe657a0a390a2df87f35320d9d1ad5e3de96858
ivanls <ivan.lacassie@gmail.com>
2026-07-07 22:06:53 +0200

    Implemented User entity and Role enum

---
+++ src/main/java/com/ivan/taskflow/entity/Role.java
@@ -0,0 +1,6 @@
+package com.ivan.taskflow.entity;
+
+public enum Role {
+    USER,
+    ADMIN
+}
---
+++ src/main/java/com/ivan/taskflow/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.ivan.taskflow.repository;
+
+import com.ivan.taskflow.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername (String username);
+}
