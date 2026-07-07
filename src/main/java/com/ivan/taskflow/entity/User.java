1791b81cddd27436d7403426870b27b2d58e8125
ivanls <ivan.lacassie@gmail.com>
2026-07-07 22:06:53 +0200

    Implemented User entity and Role enum

---
+++ src/main/java/com/ivan/taskflow/entity/User.java
@@ -0,0 +1,27 @@
+package com.ivan.taskflow.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String password; //This has to be saved encrypted
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+}
