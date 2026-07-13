4f0deaa73080ac6e2e5766e8fe209e3fd1cedd7d
ivanls <ivan.lacassie@gmail.com>
2026-07-14 00:39:19 +0200

    password encoder implemented

--- src/main/java/com/ivan/taskflow/config/SecurityConfig.java
+++ src/main/java/com/ivan/taskflow/config/SecurityConfig.java
@@ -1,8 +1,12 @@
 package com.ivan.taskflow.config;
 
+import com.ivan.taskflow.security.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,6 +19,17 @@ public class SecurityConfig {
                 .anyRequest().authenticated()
 
         ); return http.build();
+    }
+
+    private final CustomUserDetailsService customUserDetailsService;
+
+    public SecurityConfig(CustomUserDetailsService customUserDetailsService) {
+        this.customUserDetailsService = customUserDetailsService;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
---
+++ src/main/java/com/ivan/taskflow/security/CustomUserDetailsService.java
@@ -0,0 +1,34 @@
+package com.ivan.taskflow.security;
+
+import com.ivan.taskflow.entity.User;
+import com.ivan.taskflow.repository.UserRepository;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+//This class loads a user and adapts it to UserDetails
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService (UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                List.of(new SimpleGrantedAuthority("ROLE_" + user.getRole().name()))
+        );
+    }
+}
