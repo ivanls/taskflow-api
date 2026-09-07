adf8f016ceac8fd40b648588741cfcb0b898d81a
ivanls <ivan.lacassie@gmail.com>
2026-09-07 21:50:56 +0200

    fix: prevent duplicate usernames

--- src/main/java/com/ivan/taskflow/entity/Task.java
+++ src/main/java/com/ivan/taskflow/entity/Task.java
@@ -31,6 +31,6 @@ public class Task {
     private LocalDateTime createdAt;
 
     @ManyToOne
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
 }
--- src/main/java/com/ivan/taskflow/entity/User.java
+++ src/main/java/com/ivan/taskflow/entity/User.java
@@ -18,6 +18,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false, unique = true)
     private String username;
 
     private String password; //This has to be saved encrypted
